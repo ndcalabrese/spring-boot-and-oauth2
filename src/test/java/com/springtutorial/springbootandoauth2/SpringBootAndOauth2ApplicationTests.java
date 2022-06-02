c9b77e8be0ca6248405351142cb881e6e864ac9b
ndcalabrese <ndcalabrese@gmail.com>
2022-06-01 20:05:43 -0400

    Initial commit - Simple GitHub auth

---
+++ src/test/java/com/springtutorial/springbootandoauth2/SpringBootAndOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorial.springbootandoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
